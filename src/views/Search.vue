ca8472a9d37e444a3956c122e83c0d8ce94a7bd5
wendy.liu <aaa3520732@gmail.com>
2021-12-21 23:48:43 +0800

    search feat: 性別英文轉中文和預設圖片

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,53 +2,9 @@
     <div class="search_page">
         <div class="wrap_container">
             <h2>尋找浪浪</h2>
-            <div class="choose">
-                <p>動物品種</p>
-                <div class="dropdown">
-                    <a
-                        class="btn dropdown-toggle bg-white"
-                        href="#"
-                        role="button"
-                        id="dropdownMenuLink"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        請選擇
-                    </a>
-
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">犬</a></li>
-                        <li><a class="dropdown-item" href="#">貓</a></li>
-                        <li><a class="dropdown-item" href="#">鼠</a></li>
-                    </ul>
-                </div>
-                <p>收容所</p>
-                <div class="dropdown">
-                    <a
-                        class="btn dropdown-toggle bg-white"
-                        href="#"
-                        role="button"
-                        id="dropdownMenuLink"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        請選擇
-                    </a>
-
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">澎湖</a></li>
-                        <li><a class="dropdown-item" href="#">高雄</a></li>
-                        <li><a class="dropdown-item" href="#">台南</a></li>
-                    </ul>
-                </div>
-            </div>
+            <Filter @confirm="getCity"></Filter>
             <section class="content">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                <Card v-for="pet in pets" :key="pet.animal_id" :pet="pet"></Card>
             </section>
             <nav aria-label="Page navigation ">
                 <ul class="pagination">
@@ -69,18 +25,39 @@
 
 <script>
 import Card from '@/components/Card';
+import Api from '@/services/Api';
+import Filter from '@/components/Filter';
 export default {
     name: 'Search',
     components: {
         Card,
+        Filter,
+    },
+    data() {
+        return {
+            pets: null,
+            cities: [],
+        };
+    },
+    created() {
+        Api.getPets()
+            .then((response) => {
+                this.pets = response.data;
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    methods: {
+        getCity(city) {
+            console.log('parent', city);
+        },
     },
 };
 </script>
 
 <style lang="scss" scope>
-* {
-    outline: 1px solid red;
-}
 .search_page {
     background: color.$primary;
 }
@@ -96,67 +73,7 @@ h2 {
     margin-bottom: 26px;
     font-size: 50px;
 }
-.choose {
-    display: flex;
-    margin-bottom: 63px;
 
-    p {
-        margin-right: 24px;
-        font-size: 30px;
-        color: color.$text_light;
-    }
-}
-
-$border: 2px solid color.$secondary;
-.dropdown {
-    &:nth-of-type(1) {
-        margin-right: 40px;
-    }
-    & > a {
-        display: flex;
-        justify-content: space-between;
-    }
-    & > a {
-        border: $border;
-    }
-    ul {
-        border: $border;
-    }
-    li + li {
-        border-top: $border;
-    }
-    a {
-        padding: 9px 16px;
-        width: 250px;
-        color: color.$text_light;
-        &:focus {
-            box-shadow: none;
-        }
-
-        &:hover {
-            color: color.$text_light;
-        }
-        &:after {
-            align-self: center;
-            font-size: 25px;
-        }
-    }
-    .dropdown-menu {
-        padding: 0;
-        a {
-            font-size: 18px;
-            text-align: center;
-            color: color.$text_light;
-            &:hover {
-                background: color.$secondary;
-                color: #fff;
-            }
-        }
-    }
-}
-.dropdown-menu.show {
-    top: -5px !important;
-}
 .wrap_container {
     .pagination {
         justify-content: center;
