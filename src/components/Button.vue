2579c80d55cde0eb82fd46c16a305380fe94d1f1
Chiahsuan Chou <belinda05021992@gmail.com>
2021-12-21 11:03:03 +0800

    Button feat: 新增元件及定義基本樣式

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  padding: 26px 66px;
+  background-color: color.$secondary;
+  color: color.$primary;
+  border-radius: 10px;
+  font-weight: 900;
+  font-size: 25px;
+}
+</style>
