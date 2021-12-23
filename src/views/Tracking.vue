02ca4a01e29f1fc62ea0594c14aacf1dbbeb6bdd
李方傑 <lifangjie@ajiedeMacBook-Air.local>
2021-12-23 20:13:21 +0800

    localStorage 測試

--- src/views/Tracking.vue
+++ src/views/Tracking.vue
@@ -1,9 +1,41 @@
 <template>
-  <h2>追蹤浪浪頁</h2>
+  <h2>追蹤浪浪</h2>
+  <!-- <Card :pet="pet" /> -->
+  <Card v-for="pet in trackingPets" :key="pet.animal_id" :pet="pet" />
 </template>
 
 <script>
-export default {};
+import Card from "@/components/Card.vue";
+export default {
+  name: "Tracking",
+  components: { Card },
+  data() {
+    return {
+      trackingPets: [],
+      // pet: {
+      //   animal_id: 123,
+      //   animal_kind: "狗",
+      //   animal_colour: "brown",
+      //   animal_bacterin: "F",
+      //   animal_place: "tainan",
+      // },
+      // pet: {},
+    };
+  },
+  created() {
+    const pet = {
+      animal_id: 124,
+      animal_kind: "狗",
+      animal_colour: "brown",
+      animal_bacterin: "F",
+      animal_place: "tainan",
+    };
+    localStorage.setItem("favorite", JSON.stringify(pet));
+    const favorite = JSON.parse(localStorage.getItem("favorite"));
+    console.log(favorite);
+    this.trackingPets.push(favorite);
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
