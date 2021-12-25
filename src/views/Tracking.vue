9cfefb763f886121a9d6c2efa691dabba7c7a6db
wendy.liu <aaa3520732@gmail.com>
2021-12-25 13:48:41 +0800

    tracking fix: localStorage setItem

--- src/views/Tracking.vue
+++ src/views/Tracking.vue
@@ -44,17 +44,17 @@ export default {
     };
   },
   created() {
-    const pet = {
-      animal_id: 124,
-      animal_kind: "狗",
-      animal_colour: "brown",
-      animal_bacterin: "F",
-      animal_place: "tainan",
-    };
-    localStorage.setItem("favorite", JSON.stringify(pet));
-    const favorite = JSON.parse(localStorage.getItem("favorite"));
-    console.log(favorite);
-    this.trackingPets.push(favorite);
+    // const pet = {
+    //   animal_id: 124,
+    //   animal_kind: "狗",
+    //   animal_colour: "brown",
+    //   animal_bacterin: "F",
+    //   animal_place: "tainan",
+    // };
+    // localStorage.setItem("favorite", JSON.stringify(pet));
+    // const favorite = JSON.parse(localStorage.getItem("favorite"));
+    // console.log(favorite);
+    // this.trackingPets.push(favorite);
   },
 };
 </script>
