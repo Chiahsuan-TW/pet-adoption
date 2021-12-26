5cd5d2ff26f1e3ac20a501e603bcccdad6f2a253
李方傑 <lifangjie@ajiedeMacBook-Air.local>
2021-12-26 21:09:12 +0800

    RWD feat: 切navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,6 +11,42 @@
       <router-link :to="{ name: 'Info' }">資訊</router-link>
     </div>
   </nav>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link :to="{ name: 'Home' }"
+        ><h1>
+          <img src="./../assets/images/paw.png" alt="dog paw" /></h1
+      ></router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link :to="{ name: 'Search' }">尋找浪浪</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'Tracking' }">追蹤浪浪</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
